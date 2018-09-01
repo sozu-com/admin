a09cc2612a05b4dcef334c7c65b8e632a69a9c9d
cbl1002 <sheena@code-brew.com>
2018-09-01 17:06:21 +0530

    chat test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://kanguroo.mx/api/admin/',
-  socketUrl: 'http://kanguroo.mx:8080'
+  baseUrl: 'http://45.232.252.136/api/admin/',
+  socketUrl: 'http://45.232.252.136:8080'
   // baseUrl: 'https://kanguroo.mx/api/admin/',
   // socketUrl: 'https://kanguroo.mx:8080'
 };
