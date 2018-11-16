9142e81c9294ca73de28e68ee50b8846c2e17318
cbl1002 <sheena@code-brew.com>
2018-11-16 13:36:35 +0530

    chng

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -5000,15 +5000,11 @@ var AppHeaderComponent = /** @class */ (function () {
 
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
 var environment = {
-    production: false,
-    baseIP: 'http://192.168.100.88/api/',
-    baseUrl: 'http://192.168.100.88:8500/api/admin/',
-    socketUrl: 'http://192.168.100.88:8080',
+    production: true,
+    baseIP: 'https://kanguroo.mx/api/',
+    baseUrl: 'https://kanguroo.mx/api/admin/',
+    socketUrl: 'http://45.232.252.136:8080',
     deviceId: 'ADMIN'
 };
 //# sourceMappingURL=environment.js.map
