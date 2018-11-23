ddf22e4c9326e1d052723661af9c47df76ea4833
cbl1002 <sheena@code-brew.com>
2018-11-23 11:08:40 +0530

    firebase+https

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,8 +1,11 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://45.232.252.136:8002/api/admin/',
-  baseIP: 'http://45.232.252.136:8002/api/',
-  socketUrl: 'http://45.232.252.136:8090',
+  // baseUrl: 'http://45.232.252.136:8002/api/admin/',
+  // baseIP: 'http://45.232.252.136:8002/api/',
+  // socketUrl: 'http://45.232.252.136:8090',
+  baseIP: 'https://kanguroo.mx/api/',
+  baseUrl: 'https://kanguroo.mx/api/admin/',
+  socketUrl: 'http://45.232.252.136:8080',
   deviceId: 'ADMIN',
 
   firebase: {
