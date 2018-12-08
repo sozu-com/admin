8eb826d2cd0e267053f74dd3c581e42e64e195d3
cbl1002 <sheena@code-brew.com>
2018-12-08 15:06:26 +0530

    data collector building tab - prod build

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -5049,9 +5049,9 @@ var AppHeaderComponent = /** @class */ (function () {
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
 var environment = {
     production: true,
-    baseUrl: 'http://45.232.252.136:8002/api/admin/',
-    baseIP: 'http://45.232.252.136:8002/api/',
-    socketUrl: 'http://45.232.252.136:8090',
+    baseIP: 'http://kanguroo.mx/api/',
+    baseUrl: 'http://kanguroo.mx/api/admin/',
+    socketUrl: 'http://45.232.252.136:8080',
     deviceId: 'ADMIN',
     firebase: {
         apiKey: 'AIzaSyAZMJdN4-tFi3vkKLz-c-BoFHMq7eAkoAc',
