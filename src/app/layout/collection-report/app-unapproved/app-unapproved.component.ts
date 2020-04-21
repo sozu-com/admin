7dabefefff1517e58a235d0438f97fea675ab6b4
Gaurav singla <g.singla991@gmail.com>
2020-04-22 01:37:24 +0530

    report and feedback

--- src/app/layout/collection-report/app-unapproved/app-unapproved.component.ts
+++ src/app/layout/collection-report/app-unapproved/app-unapproved.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import * as CanvasJS from 'src/assets/js/canvasjs.min.js';
+import { IProperty } from 'src/app/common/property';
+import { AdminService } from 'src/app/services/admin.service';
+import * as moment from 'moment';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { TranslateService } from '@ngx-translate/core';
+import { CollectionReport } from 'src/app/models/collection-report.model';
 
 @Component({
   selector: 'app-app-unapproved',
@@ -7,9 +14,222 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppUnapprovedComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  public parameter: IProperty = {};
+  singleDropdownSettings: any;
+  multiDropdownSettings: any;
+  input: CollectionReport;
+  projects: Array<any>;
+  selctedProjects: Array<any>;
+  developers: Array<any>;
+  selectedDevelopers: Array<any>;
+  colorScheme = {
+    domain: ['#ee7b7c', '#f5d05c']
+  };
+  locale: any;
+  today = new Date();
+  constructor(public admin: AdminService, 
+    private spinner: NgxSpinnerService,
+    private translate: TranslateService) {
   }
 
+  onSelect(event) {}
+
+  ngOnInit() {
+    this.input = new CollectionReport();
+    this.input.start_date = moment().subtract(6, 'months').toDate();
+    this.input.end_date = moment().toDate();
+    this.iniDropDownSetting()
+    this.getDevelopers();
+
+    this.locale = {
+      firstDayOfWeek: 0,
+      dayNames: ['domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado'],
+      dayNamesShort: ['dom', 'lun', 'mar', 'mié', 'jue', 'vie', 'sáb'],
+      dayNamesMin: ['D', 'L', 'M', 'X', 'J', 'V', 'S'],
+      monthNames: ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio',
+        'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'],
+      monthNamesShort: ['ene', 'feb', 'mar', 'abr', 'may', 'jun',
+        'jul', 'ago', 'sep', 'oct', 'nov', 'dic'],
+      today: 'Hoy',
+      clear: 'Clara',
+      dateFormat: 'mm/dd/yy',
+      weekHeader: 'Wk'
+    };
+    this.getReportData();
+  }
+
+
+  iniDropDownSetting() {
+    this.singleDropdownSettings = {
+      singleSelection: true,
+      idField: 'id',
+      textField: 'name',
+      selectAllText: this.translate.instant('commonBlock.selectAll'),
+      unSelectAllText: this.translate.instant('commonBlock.unselectAll'),
+      searchPlaceholderText: this.translate.instant('commonBlock.search'),
+      allowSearchFilter: true
+    };
+    this.multiDropdownSettings = {
+      singleSelection: false,
+      idField: 'id',
+      textField: 'name',
+      selectAllText: this.translate.instant('commonBlock.selectAll'),
+      unSelectAllText: this.translate.instant('commonBlock.unselectAll'),
+      searchPlaceholderText: this.translate.instant('commonBlock.search'),
+      allowSearchFilter: true
+    };
+  }
+
+  onItemDeSelect(arrayNAme: any, obj: any) {
+    this[arrayNAme].push(obj);
+  }
+
+  onItemSelect(param:any, obj: any) {
+    this[param].push(obj);
+  }
+
+  onSelectAll(obj: any) {
+  }
+
+  
+  getDevelopers() {
+    this.admin.postDataApi('getUnblockedDevelopers', {})
+      .subscribe(
+        success => {
+          this.developers = success.data;
+        }, error => {
+          this.spinner.hide();
+        }
+      );
+  }
+
+  onSelectDeveloper(isSelected: number, obj: any) {
+    if (isSelected) {
+      this.searchBuilding(obj.id);
+    } else {
+      this.projects = [];
+      this.selctedProjects = [];
+    }
+  }
+
+  searchBuilding(developer_id: string) {
+    this.spinner.show();
+    const input = {
+      developer_id: developer_id
+    };
+    this.admin.postDataApi('getUnblockedProjects', input)
+      .subscribe(
+        success => {
+          this.spinner.hide();
+          this.projects = success['data'];
+        },
+        error => {
+          this.spinner.hide();
+        }
+      );
+  }
+
+
+  getReportData () {
+    const input: any = JSON.parse(JSON.stringify(this.input));
+    input.start_date = moment(this.input.start_date).format('YYYY-MM-DD');
+    input.end_date = moment(this.input.end_date).format('YYYY-MM-DD');
+
+    if (this.selctedProjects) {
+      const d = this.selctedProjects.map(o => o.id);
+      input.building_id = d;
+    }
+    if (this.selectedDevelopers) {
+      const d = this.selectedDevelopers.map(o => o.id);
+      input.developer_id = d;
+    }
+
+    this.spinner.show();
+    this.admin.postDataApi('reports/bank', input).subscribe(r => {
+      this.spinner.hide();
+      this.plotData();
+    }, error => {
+      this.spinner.hide();
+    });
+  }
+
+  plotData() {
+
+    var chart = new CanvasJS.Chart("chartContainer", {
+      animationEnabled: true,
+      title:{
+        text: "Crude Oil Reserves vs Production, 2016"
+      },	
+      axisY: {
+        title: "Billions of Barrels",
+        titleFontColor: "#4F81BC",
+        lineColor: "#4F81BC",
+        labelFontColor: "#4F81BC",
+        tickColor: "#4F81BC"
+      },
+      axisY2: {
+        title: "Millions of Barrels/day",
+        titleFontColor: "#C0504E",
+        lineColor: "#C0504E",
+        labelFontColor: "#C0504E",
+        tickColor: "#C0504E"
+      },	
+      toolTip: {
+        shared: true
+      },
+      legend: {
+        cursor:"pointer",
+        itemclick: toggleDataSeries
+      },
+      data: [{
+        type: "column",
+        name: "Proven Oil Reserves (bn)",
+        legendText: "Proven Oil Reserves",
+        showInLegend: true, 
+        dataPoints:[
+          { label: "Saudi", y: 266.21 },
+          { label: "Venezuela", y: 302.25 },
+          { label: "Iran", y: 157.20 },
+          { label: "Iraq", y: 148.77 },
+          { label: "Kuwait", y: 101.50 },
+          { label: "UAE", y: 97.8 }
+        ]
+      },
+      {
+        type: "column",	
+        name: "Oil Production (million/day)",
+        legendText: "Oil Production",
+        axisYType: "secondary",
+        showInLegend: true,
+        dataPoints:[
+          { label: "Saudi", y: 10.46 },
+          { label: "Venezuela", y: 2.27 },
+          { label: "Iran", y: 3.99 },
+          { label: "Iraq", y: 4.45 },
+          { label: "Kuwait", y: 2.92 },
+          { label: "UAE", y: 3.1 }
+        ]
+      }]
+    });
+    chart.render();
+    
+    function toggleDataSeries(e) {
+      if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+        e.dataSeries.visible = false;
+      }
+      else {
+        e.dataSeries.visible = true;
+      }
+      chart.render();
+    }
+        
+  }
+
+  resetFilters() {
+    this.input = new CollectionReport();
+    this.input.start_date = moment().subtract(6, 'months').toDate();
+    this.input.end_date = moment().toDate();
+    this.selectedDevelopers = [];
+    this.selctedProjects = [];
+  }
 }
--- src/app/layout/collection-report/sales-trend/sales-trend.component.ts
+++ src/app/layout/collection-report/sales-trend/sales-trend.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import * as CanvasJS from 'src/assets/js/canvasjs.min.js';
+import { IProperty } from 'src/app/common/property';
+import { AdminService } from 'src/app/services/admin.service';
+import * as moment from 'moment';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { TranslateService } from '@ngx-translate/core';
+import { CollectionReport } from 'src/app/models/collection-report.model';
 
 @Component({
   selector: 'app-sales-trend',
@@ -7,9 +14,194 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SalesTrendComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  public parameter: IProperty = {};
+  singleDropdownSettings: any;
+  multiDropdownSettings: any;
+  input: CollectionReport;
+  projects: Array<any>;
+  selctedProjects: Array<any>;
+  developers: Array<any>;
+  selectedDevelopers: Array<any>;
+  colorScheme = {
+    domain: ['#ee7b7c', '#f5d05c']
+  };
+  locale: any;
+  today = new Date();
+  constructor(public admin: AdminService, 
+    private spinner: NgxSpinnerService,
+    private translate: TranslateService) {
   }
 
+  onSelect(event) {}
+
+  ngOnInit() {
+    this.input = new CollectionReport();
+    this.input.start_date = moment().subtract(6, 'months').toDate();
+    this.input.end_date = moment().toDate();
+    this.iniDropDownSetting()
+    this.getDevelopers();
+
+    this.locale = {
+      firstDayOfWeek: 0,
+      dayNames: ['domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado'],
+      dayNamesShort: ['dom', 'lun', 'mar', 'mié', 'jue', 'vie', 'sáb'],
+      dayNamesMin: ['D', 'L', 'M', 'X', 'J', 'V', 'S'],
+      monthNames: ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio',
+        'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'],
+      monthNamesShort: ['ene', 'feb', 'mar', 'abr', 'may', 'jun',
+        'jul', 'ago', 'sep', 'oct', 'nov', 'dic'],
+      today: 'Hoy',
+      clear: 'Clara',
+      dateFormat: 'mm/dd/yy',
+      weekHeader: 'Wk'
+    };
+    this.getReportData();
+  }
+
+
+  iniDropDownSetting() {
+    this.singleDropdownSettings = {
+      singleSelection: true,
+      idField: 'id',
+      textField: 'name',
+      selectAllText: this.translate.instant('commonBlock.selectAll'),
+      unSelectAllText: this.translate.instant('commonBlock.unselectAll'),
+      searchPlaceholderText: this.translate.instant('commonBlock.search'),
+      allowSearchFilter: true
+    };
+    this.multiDropdownSettings = {
+      singleSelection: false,
+      idField: 'id',
+      textField: 'name',
+      selectAllText: this.translate.instant('commonBlock.selectAll'),
+      unSelectAllText: this.translate.instant('commonBlock.unselectAll'),
+      searchPlaceholderText: this.translate.instant('commonBlock.search'),
+      allowSearchFilter: true
+    };
+  }
+
+  onItemDeSelect(arrayNAme: any, obj: any) {
+    this[arrayNAme].push(obj);
+  }
+
+  onItemSelect(param:any, obj: any) {
+    this[param].push(obj);
+  }
+
+  onSelectAll(obj: any) {
+  }
+
+  
+  getDevelopers() {
+    this.admin.postDataApi('getUnblockedDevelopers', {})
+      .subscribe(
+        success => {
+          this.developers = success.data;
+        }, error => {
+          this.spinner.hide();
+        }
+      );
+  }
+
+  onSelectDeveloper(isSelected: number, obj: any) {
+    if (isSelected) {
+      this.searchBuilding(obj.id);
+    } else {
+      this.projects = [];
+      this.selctedProjects = [];
+    }
+  }
+
+  searchBuilding(developer_id: string) {
+    this.spinner.show();
+    const input = {
+      developer_id: developer_id
+    };
+    this.admin.postDataApi('getUnblockedProjects', input)
+      .subscribe(
+        success => {
+          this.spinner.hide();
+          this.projects = success['data'];
+        },
+        error => {
+          this.spinner.hide();
+        }
+      );
+  }
+
+
+  getReportData () {
+    const input: any = JSON.parse(JSON.stringify(this.input));
+    input.start_date = moment(this.input.start_date).format('YYYY-MM-DD');
+    input.end_date = moment(this.input.end_date).format('YYYY-MM-DD');
+
+    if (this.selctedProjects) {
+      const d = this.selctedProjects.map(o => o.id);
+      input.building_id = d;
+    }
+    if (this.selectedDevelopers) {
+      const d = this.selectedDevelopers.map(o => o.id);
+      input.developer_id = d;
+    }
+
+    this.spinner.show();
+    this.admin.postDataApi('reports/bank', input).subscribe(r => {
+      this.spinner.hide();
+      this.plotData();
+    }, error => {
+      this.spinner.hide();
+    });
+  }
+
+  plotData() {
+
+    var chart = new CanvasJS.Chart("chartContainer", {
+      animationEnabled: true,  
+      title:{
+        text: "Music Album Sales by Year"
+      },
+      axisY: {
+        title: "Units Sold",
+        valueFormatString: "#0,,.",
+        suffix: "mn",
+        stripLines: [{
+          value: 3366500,
+          label: "Average"
+        }]
+      },
+      data: [{
+        yValueFormatString: "#,### Units",
+        xValueFormatString: "YYYY",
+        type: "spline",
+        dataPoints: [
+          {x: new Date(2002, 0), y: 2506000},
+          {x: new Date(2003, 0), y: 2798000},
+          {x: new Date(2004, 0), y: 3386000},
+          {x: new Date(2005, 0), y: 6944000},
+          {x: new Date(2006, 0), y: 6026000},
+          {x: new Date(2007, 0), y: 2394000},
+          {x: new Date(2008, 0), y: 1872000},
+          {x: new Date(2009, 0), y: 2140000},
+          {x: new Date(2010, 0), y: 7289000},
+          {x: new Date(2011, 0), y: 4830000},
+          {x: new Date(2012, 0), y: 2009000},
+          {x: new Date(2013, 0), y: 2840000},
+          {x: new Date(2014, 0), y: 2396000},
+          {x: new Date(2015, 0), y: 1613000},
+          {x: new Date(2016, 0), y: 2821000},
+          {x: new Date(2017, 0), y: 2000000}
+        ]
+      }]
+    });
+    chart.render();
+    
+      }
+
+  resetFilters() {
+    this.input = new CollectionReport();
+    this.input.start_date = moment().subtract(6, 'months').toDate();
+    this.input.end_date = moment().toDate();
+    this.selectedDevelopers = [];
+    this.selctedProjects = [];
+  }
 }
