4b753c6b32298a1989df95d9316acfd04a495957
Gaurav singla <g.singla991@gmail.com>
2020-07-28 19:14:30 +0530

    bufs

--- src/app/directives/upto-two-digit-decimal-number.directive.ts
+++ src/app/directives/upto-two-digit-decimal-number.directive.ts
@@ -8,12 +8,14 @@ export class UptoTwoDigitDecimalNumberDirective {
   private regex: RegExp = new RegExp(/^\d*\.?\d{0,1}$/g);
   // Allow key codes for special events. Reflect :
   // Backspace, tab, end, home
+  
   private specialKeys: Array<string> = ['.', 'Backspace', 'Tab', 'End', 'Home', '-', 'ArrowLeft', 'ArrowRight', 'Del', 'Delete'];
 
   constructor(private el: ElementRef) {
   }
   @HostListener('keydown', ['$event'])
   onKeyDown(event: KeyboardEvent) {
+    
     // console.log(this.el.nativeElement.value);
     // console.log(this.specialKeys.indexOf(event.key), event.key);
     // Allow Backspace, tab, end, and home keys
@@ -22,6 +24,7 @@ export class UptoTwoDigitDecimalNumberDirective {
     }
     let current: string = this.el.nativeElement.value;
     const position = this.el.nativeElement.selectionStart;
+    // console.log(current)
     // console.log(position)
     const next: string = [current.slice(0, position), event.key == 'Decimal' ? '.' : event.key, current.slice(position)].join('');
     if (next && !String(next).match(this.regex)) {
