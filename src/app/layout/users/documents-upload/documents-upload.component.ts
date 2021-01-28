5596283bf781c1221fde0079472fb5c356388fa4
soma <soma.rani@codebrewinnovations.com>
2021-01-28 13:04:17 +0530

    doc

--- src/app/layout/users/documents-upload/documents-upload.component.ts
+++ src/app/layout/users/documents-upload/documents-upload.component.ts
@@ -11,7 +11,7 @@ import { CommonService } from 'src/app/services/common.service';
 import { AdminService } from 'src/app/services/admin.service';
 import { TranslateService } from '@ngx-translate/core';
 import { LegalRepresentative, Banks } from 'src/app/models/legalEntity.model';
-import { IMarritalStatus } from 'src/app/common/marrital-status-interface';
+import { ToastrService } from 'ngx-toastr';
 declare const google;
 declare let swal: any;
 
@@ -24,7 +24,12 @@ declare let swal: any;
 export class DocumentsUploadComponent implements OnInit {
   @ViewChild('mapDiv') mapDiv: ElementRef;
   @ViewChild('search') searchElementRef: ElementRef;
+  @ViewChild('docsModalClose') docsModalClose: ElementRef;
+  @ViewChild('docsFile') docsFile: ElementRef;
   public parameter: IProperty = {};
+  @ViewChild('docsModalOpen') docsModalOpen: ElementRef;
+  @ViewChild('folderModalOpen') folderModalOpen: ElementRef;
+  @ViewChild('folderModalClose') folderModalClose: ElementRef;
   initialCountry: any;
   show = false;
   image: any;
@@ -35,7 +40,15 @@ export class DocumentsUploadComponent implements OnInit {
   language_code: string;
   showInput: boolean = false;
   dataNotAvailable: boolean;
-
+  collectionFolders: Array<any>;
+  folderIndex: number;
+  folderName: string;
+  docs: any;
+  folderId: number;
+  docFile: any;
+  docsName: any;
+  mode: string;
+  selectedFolder: any = {};
   constructor(
     public constant: Constant,
     private cs: CommonService,
@@ -45,7 +58,7 @@ export class DocumentsUploadComponent implements OnInit {
     private route: ActivatedRoute,
     private spinner: NgxSpinnerService,
     private translate: TranslateService,
-    private router: Router
+    private router: Router,  private toastr: ToastrService,
   ) { }
 
   ngOnInit() {
@@ -158,4 +171,64 @@ export class DocumentsUploadComponent implements OnInit {
         });
   }
   
+
+  saveCollectionFolders() {
+    this.spinner.show();
+    this.admin.postDataApi('addCollection', {id: this.model.id, step: 6, 'collection_folders': this.collectionFolders})
+      .subscribe(
+        success => {
+          this.spinner.hide();
+          // swal({
+          //   html: this.translate.instant('message.success.submittedSccessfully'), type: 'success'
+          // });
+          this.router.navigate(['/dashboard/collections/view-collections']);
+        }, error => {
+          this.spinner.hide();
+        }
+      );
+  }
+
+  modelOpenFun(folder, index: number) {
+    this.folderIndex = index;
+    this.folderName = folder.name;
+    this.docs = folder.folder_docs;
+    this.folderId = folder.id;
+    this.docsModalOpen.nativeElement.click();
+  }
+
+  closeModal() {
+    this.docsModalClose.nativeElement.click();
+  }
+
+  onSelectFile(files,folderId) {
+    this.parameter.loading = true;
+    this.cs.saveAttachment1(files[0],folderId).subscribe(
+      success => {
+        this.parameter.loading = false;
+        this.docFile = success['data'].name;
+        this.folderId = success['data'].id;
+      }, error => {
+        this.parameter.loading = false;
+      }
+    );
+  }
+
+
+  addDocs() {
+    if (!this.docFile) {
+      this.toastr.clear();
+      this.toastr.error(this.translate.instant('message.error.pleaseEnterDocuFile'), this.translate.instant('swal.error'));
+      return;
+    }
+    this.docs.push({display_name: this.docFile});
+    this.docFile = ''; 
+    this.docsFile.nativeElement.value = '';
+  }
+
+  openAddFolder(folder, index: number) {
+     this.folderName = folder.user_document.name_en;
+    this.folderId = folder.id;
+    this.folderModalOpen.nativeElement.click();
+  }
+
 }
