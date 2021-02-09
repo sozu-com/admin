e39215e3ffefc9d6ab2234b68039c07d9ba9f64d
soma <soma.rani@codebrewinnovations.com>
2021-02-09 18:31:14 +0530

    credit section

--- src/app/layout/credit/credit-add-edit/credit-add-edit.component.ts
+++ src/app/layout/credit/credit-add-edit/credit-add-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-credit-add-edit',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreditAddEditComponent implements OnInit {
 
-  constructor() { }
+  constructor( private spinner: NgxSpinnerService,) { }
 
   ngOnInit() {
+    this.getListing();
+    this.spinner.hide();
   }
-
+  getListing() {
+  //  this.spinner.show();
+  }
 }
