decb57b7db71528df54ed411a9041a0314a68465
soma <soma.rani@codebrewinnovations.com>
2021-02-19 09:59:28 +0530

    legal entity ui change

--- src/app/layout/legal-entity/legal-entity.module.ts
+++ src/app/layout/legal-entity/legal-entity.module.ts
@@ -19,8 +19,6 @@ import { SharedModule } from 'src/app/modules/shared.module';
 import { LegalEntityComponent } from './legal-entity.component';
 import { AddLegalEntityComponent } from './add-legal-entity/add-legal-entity.component';
 import { DocumentUploadComponent } from './document-upload/document-upload.component';
-import { EditLegalEntityComponent } from './edit-legal-entity/edit-legal-entity.component';
-
 const routes: Routes = [
   {
     path: 'view-all', component: LegalEntityComponent,
@@ -31,10 +29,6 @@ const routes: Routes = [
   },
   {
     path: 'add-legal-entity/:id', component: AddLegalEntityComponent,
-    canActivate: [AclUserGuard], data: { roles: ['Manage Legal Entity', 'can_read', ''] }
-  },
-  {
-    path: 'edit-legal-entity/:id', component: EditLegalEntityComponent,
     canActivate: [AclUserGuard], data: { roles: ['Manage Legal Entity', 'can_read', ''] }
   },
   {
@@ -65,8 +59,7 @@ const routes: Routes = [
   declarations: [
     LegalEntityComponent,
     AddLegalEntityComponent,
-    DocumentUploadComponent,
-    EditLegalEntityComponent
+    DocumentUploadComponent
   ]
 })
 
