e806f1514d97ceec960048ac52fc938bc28aaf45
Rajat Kumar <rajat.kumar@codebrewinnovations.com>
2021-03-11 19:12:17 +0530

    bug fixed

--- src/app/layout/collection-report/arrear-report/arrear-report.component.ts
+++ src/app/layout/collection-report/arrear-report/arrear-report.component.ts
@@ -346,7 +346,7 @@ export class ArrearReportComponent implements OnInit {
             Currency: item.code || '',
             'concept': item.NAME || '',
             'date': item.date || '',
-            'paymentTotal': item.symbol + (item.amount || 0),
+            'paymentTotal': item.symbol + (((item.amount || 0) + (item.penelty || 0)) - (item.calc_payment_amount || 0)),
             'totalArrear': item.symbol + (item.total_amount || 0)
           });
         }
