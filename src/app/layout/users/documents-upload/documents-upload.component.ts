fab36a35c0eb62972466ded366f9983d87e1e7ff
Deepak Jaiswal <deepak.jaiswal@codebrewinnovations.com>
2021-03-19 17:56:40 +0530

    505-F and 506-F

--- src/app/layout/users/documents-upload/documents-upload.component.ts
+++ src/app/layout/users/documents-upload/documents-upload.component.ts
@@ -1,20 +1,14 @@
-import { Component, OnInit, NgZone, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { NgForm } from '@angular/forms';
 import { Users } from 'src/app/models/users.model';
-import { MapsAPILoader } from '@agm/core';
 import { FileUpload } from 'src/app/common/fileUpload';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { IProperty } from 'src/app/common/property';
 import { Constant } from 'src/app/common/constants';
-import { CommonService } from 'src/app/services/common.service';
 import { AdminService } from 'src/app/services/admin.service';
 import { TranslateService } from '@ngx-translate/core';
-import { LegalRepresentative, Banks } from 'src/app/models/legalEntity.model';
 import { ToastrService } from 'ngx-toastr';
-declare const google;
 declare let swal: any;
-
 
 @Component({
   selector: 'app-documents-upload',
@@ -22,6 +16,7 @@ declare let swal: any;
   styleUrls: ['./documents-upload.component.css']
 })
 export class DocumentsUploadComponent implements OnInit {
+
   @ViewChild('mapDiv') mapDiv: ElementRef;
   @ViewChild('search') searchElementRef: ElementRef;
   @ViewChild('docsModalClose') docsModalClose: ElementRef;
@@ -31,8 +26,6 @@ export class DocumentsUploadComponent implements OnInit {
   @ViewChild('folderModalOpen') folderModalOpen: ElementRef;
   @ViewChild('folderModalClose') folderModalClose: ElementRef;
 
-  // initialCountry: any;
-  //show = false;
   image: any;
   file4: FileUpload;
   developer_image: any;
@@ -40,7 +33,6 @@ export class DocumentsUploadComponent implements OnInit {
   location: IProperty = {};
   language_code: string;
   showInput: boolean = false;
-  dataNotAvailable: boolean;
   collectionFolders: Array<any>;
   folderIndex: number;
   folderName: string;
@@ -51,15 +43,15 @@ export class DocumentsUploadComponent implements OnInit {
   mode: string;
   selectedFolder: any = {};
 
-  routeDetaills: { userId: string, beneficiaryId: string, tutorId: string }
+  public currentDocumentIndex: number = 0; // bydefault 0 , 1 for user , 2 for beneficiary , 3 for tutor
+  public linked_documents: Array<any>;
+  private routeDetaills: { userId: string, beneficiaryId: string, tutorId: string }
     = { userId: null, beneficiaryId: null, tutorId: null };
-  currentDocumentIndex: number = 0; // bydefault 0 , 1 for user , 2 for beneficiary , 3 for tutor
-  linked_documents: Array<any>;
-  isDeletedDocument: boolean = false;
+  private isDeletedDocument: boolean = false;
+  // private isShowSpinner: boolean = true;
 
   constructor(
     public constant: Constant,
-    private cs: CommonService,
     public admin: AdminService,
     private route: ActivatedRoute,
     private spinner: NgxSpinnerService,
@@ -70,10 +62,9 @@ export class DocumentsUploadComponent implements OnInit {
 
   ngOnInit(): void {
     this.language_code = localStorage.getItem('language_code');
-    this.initModel();
+    this.model = new Users();
     this.parameter.itemsPerPage = this.constant.itemsPerPage;
     this.parameter.p = this.constant.p;
-    //console.log(this.model, "model")
     this.parameter.sub = this.route.params.subscribe(params => {
       if (params['userId']) {
         this.model.id = params['userId'];
@@ -94,31 +85,23 @@ export class DocumentsUploadComponent implements OnInit {
     });
   }
 
-  initModel() {
-    //this.initialCountry = { initialCountry: this.constant.country_code };
-    this.model = new Users();
-    // this.model.legal_rep_banks = new Array();
-    // this.model.user_linked_documents = new Array();
-    // this.model.legal_representative = new LegalRepresentative();
-    // this.model.country_code = this.constant.country_code;
-    // this.model.dial_code = this.constant.dial_code;
-  }
-
-
-
   getUserById = (id: string): void => {
+    // if (this.isShowSpinner || this.isDeletedDocument) {
+    //   this.spinner.show();
+    // } else {
+    //   this.parameter.loading = true;
+    // }
     this.spinner.show();
     this.admin.postDataApi('getUserById', { 'user_id': id }).subscribe((success) => {
+      // if (this.isShowSpinner || this.isDeletedDocument) {
+      //   this.spinner.hide();
+      //   this.isShowSpinner = false;
+      // } else {
+      //   this.parameter.loading = false;
+      // }
       this.spinner.hide();
       this.model = new Users();
       this.model = success.data;
-      // // this.model.legal_representative = new LegalRepresentative();
-      // this.model.legal_rep_banks = success.data.legal_rep_banks;
-      // this.model.user_linked_documents = success.data.user_linked_documents;
-      // //console.log(this.model.user_linked_documents, "doc")
-      // this.model.legal_representative = success.data.legal_representative || new LegalRepresentative();
-      // this.model.legal_representative.legal_rep_banks = success.data.legal_representative.legal_rep_banks; // Array(new Banks());
-      // this.image = this.model.image;
       switch (this.currentDocumentIndex) {
         case 1:
           this.linked_documents = success.data.user_linked_documents || [];
@@ -146,22 +129,16 @@ export class DocumentsUploadComponent implements OnInit {
       }
     }, (error) => {
       this.spinner.hide();
+      // this.parameter.loading = false;
+      // this.isShowSpinner = false;
     });
-  }
-
-  // set() {
-  //   this.show = true;
-  // }
-
-  noDataAvailable(data) {
-    this.dataNotAvailable = data ? true : false;
   }
 
   viewDocument = (documentLink: string): void => {
     window.open(documentLink, '_blank');
   }
 
-  deletePopup(index: number, id: string) {
+  deletePopup = (index: number, id: string): void => {
     this.parameter.index = index;
     this.parameter.text = this.translate.instant('message.error.wantToDeleteFile');
     swal({
@@ -191,122 +168,111 @@ export class DocumentsUploadComponent implements OnInit {
   }
 
   deleteBuyerSeller = (index: number, id: string): void => {
-    this.parameter.loading = true;
-    this.parameter.index = index;
-    const input = new FormData();
-    input.append('id', id);
-    this.admin.postDataApi('deleteLinkedDocument', input).subscribe((success) => {
-      this.parameter.loading = false;
-      this.isDeletedDocument = true;
-      this.getUserById(this.model.id);
-      //this.parameter.items.splice(index, 1);
-    });
+    this.spinner.show();
+    this.admin.postDataApi('deleteLinkedDocument', this.getPostRequestToDeleteDocument(index, id)).
+      subscribe((success) => {
+        this.spinner.hide();
+        this.getUserByIdAfterDeteleDocument();
+      });
   }
 
   deleteBeneficiaryDocument = (index: number, id: string): void => {
-    this.parameter.loading = true;
-    this.parameter.index = index;
-    const input = new FormData();
-    input.append('id', id);
-    this.admin.postDataApi('deleteBeneficiaryDocument', input).subscribe((success) => {
-      this.parameter.loading = false;
-      this.isDeletedDocument = true;
-      this.getUserById(this.model.id);
-    });
+    this.spinner.show();
+    this.admin.postDataApi('deleteBeneficiaryDocument', this.getPostRequestToDeleteDocument(index, id)).
+      subscribe((success) => {
+        this.spinner.hide();
+        this.getUserByIdAfterDeteleDocument();
+      });
   }
 
   deleteTutorDocument = (index: number, id: string): void => {
-    this.parameter.loading = true;
-    this.parameter.index = index;
-    const input = new FormData();
-    input.append('id', id);
-    this.admin.postDataApi('deleteTutorDocument', input).subscribe((success) => {
-      this.parameter.loading = false;
-      this.isDeletedDocument = true;
-      this.getUserById(this.model.id);
-    });
+    this.spinner.show();
+    this.admin.postDataApi('deleteTutorDocument', this.getPostRequestToDeleteDocument(index, id)).
+      subscribe((success) => {
+        this.spinner.hide();
+        this.getUserByIdAfterDeteleDocument();
+      });
   }
 
-  closeModal() {
+  getPostRequestToDeleteDocument = (index: number, id: string): FormData => {
+    this.parameter.index = index;
+    const post = new FormData();
+    post.append('id', id);
+    return post;
+  }
+
+  getUserByIdAfterDeteleDocument = (): void => {
+    this.isDeletedDocument = true;
+    this.getUserById(this.model.id);
+  }
+
+
+  closeModal = (): void => {
     this.docFile = '';
     this.docsFile.nativeElement.value = '';
     this.docsModalClose.nativeElement.click();
   }
 
-  onSelectFile(files, folderId) {
+  onSelectFile = (files: any, folderId: number): void => {
     this.docFile = files[0];
     this.folderId = folderId;
-    //this.parameter.loading = true;
-    // this.cs.saveAttachment1(files[0], folderId).subscribe(
-    //   success => {
-    //     this.parameter.loading = false;
-    //     this.docFile = success['data'].name;
-    //     this.folderId = success['data'].id;
-    //   }, error => {
-    //     this.parameter.loading = false;
-    //   }
-    // );
   }
 
 
-  addDocs() {
+  addDocs = (): void => {
     if (!this.docFile) {
       this.toastr.clear();
       this.toastr.error(this.translate.instant('message.error.pleaseEnterDocuFile'), this.translate.instant('swal.error'));
-      return;
-    }
-
-    switch (this.currentDocumentIndex) {
-      case 1:
-        this.parameter.loading = true;
-        this.cs.saveAttachment1(this.docFile, this.folderId).subscribe((success) => {
-          this.parameter.loading = false;
-          // this.docFile = success['data'].name;
-          // this.folderId = success['data'].id;
-          this.docs.push({ display_name: this.docFile });
-          this.docFile = '';
-          this.docsFile.nativeElement.value = '';
-          this.getUserById(this.model.id);
-        }, (error) => {
-          this.parameter.loading = false;
-        });
-        break;
-      case 2:
-        const input2 = new FormData();
-        input2.append('attachment', this.docFile);
-        input2.append('id', this.folderId.toString());
-        this.parameter.loading = true;
-        this.admin.postDataApi('addBeneficiaryDocument', input2).subscribe((success) => {
-          this.parameter.loading = false;
-          this.docs.push({ display_name: this.docFile });
-          this.docFile = '';
-          this.docsFile.nativeElement.value = '';
-          this.getUserById(this.model.id);
-        }, (error) => {
-          this.parameter.loading = false;
-        });
-        break;
-      case 3:
-        const input3 = new FormData();
-        input3.append('attachment', this.docFile);
-        input3.append('id', this.folderId.toString());
-        this.parameter.loading = true;
-        this.admin.postDataApi('addTutorDocument', input3).subscribe((success) => {
-          this.parameter.loading = false;
-          this.docs.push({ display_name: this.docFile });
-          this.docFile = '';
-          this.docsFile.nativeElement.value = '';
-          this.getUserById(this.model.id);
-        }, (error) => {
-          this.parameter.loading = false;
-        });
-        break;
-      default:
-        break;
+    } else {
+      switch (this.currentDocumentIndex) {
+        case 1:
+          this.parameter.loading = true;
+          this.admin.postDataApi('addLinkedDocument', this.getPostRequestToAddDocument()).subscribe((success) => {
+            this.parameter.loading = false;
+            this.getUserByIdAfterAddDocument();
+          }, (error) => {
+            this.parameter.loading = false;
+          });
+          break;
+        case 2:
+          this.parameter.loading = true;
+          this.admin.postDataApi('addBeneficiaryDocument', this.getPostRequestToAddDocument()).subscribe((success) => {
+            this.parameter.loading = false;
+            this.getUserByIdAfterAddDocument();
+          }, (error) => {
+            this.parameter.loading = false;
+          });
+          break;
+        case 3:
+          this.parameter.loading = true;
+          this.admin.postDataApi('addTutorDocument', this.getPostRequestToAddDocument()).subscribe((success) => {
+            this.parameter.loading = false;
+            this.getUserByIdAfterAddDocument();
+          }, (error) => {
+            this.parameter.loading = false;
+          });
+          break;
+        default:
+          break;
+      }
     }
   }
 
-  openAddFolder(folder, index: number) {
+  getPostRequestToAddDocument = (): FormData => {
+    const post = new FormData();
+    post.append('attachment', this.docFile);
+    post.append('id', this.folderId.toString());
+    return post;
+  }
+
+  getUserByIdAfterAddDocument = (): void => {
+    this.docs.push({ display_name: this.docFile });
+    this.docFile = '';
+    this.docsFile.nativeElement.value = '';
+    this.getUserById(this.model.id);
+  }
+
+  openAddFolder = (folder: any): void => {
     switch (this.currentDocumentIndex) {
       case 1:
         this.folderName = this.language_code == 'en' ? folder.user_document.name_en : folder.user_document.name_es;
@@ -324,7 +290,7 @@ export class DocumentsUploadComponent implements OnInit {
     this.folderModalOpen.nativeElement.click();
   }
 
-  goBack = (): void => {
+  goBackToUserListing = (): void => {
     this.router.navigate(['/dashboard/users/edit-user', this.model.id])
   }
 
