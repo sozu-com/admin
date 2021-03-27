3544f1490436ee7a57e868ab49868f071054760b
soma <soma.rani@codebrewinnovations.com>
2021-03-27 17:11:33 +0530

    534-535-f ui

--- src/app/layout/collection-report/commission-income/commission-income.component.ts
+++ src/app/layout/collection-report/commission-income/commission-income.component.ts
@@ -18,7 +18,7 @@ const EXCEL_EXTENSION = '.xlsx';
   styleUrls: ['./commission-income.component.css']
 })
 export class CommissionIncomeComponent implements OnInit {
-
+  public navbarOpen : boolean = false;
   public parameter: IProperty = {};
   singleDropdownSettings: any;
   multiDropdownSettings: any;
@@ -340,6 +340,8 @@ export class CommissionIncomeComponent implements OnInit {
   }
 
   getCommission(item, index) {
+    this.navbarOpen = !this.navbarOpen;
+    console.log(this.navbarOpen,"this.navbarOpen")
     if(index != this.already_index){
       this.already_index = index;
       this.commissions = [];
