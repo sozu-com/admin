3b1f656717b9916f3a0852926b96826bdce866fa
Deepak Jaiswal <deepak.jaiswal@codebrewinnovations.com>
2021-05-14 16:02:10 +0530

    fix horizontal scroll in arrear report,make multiple page in project-details pdf,pdf of property-details

--- src/app/layout/properties/property-details/property-details.component.ts
+++ src/app/layout/properties/property-details/property-details.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router} from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { IProperty } from 'src/app/common/property';
 import { AdminService } from 'src/app/services/admin.service';
 import { Constant } from 'src/app/common/constants';
 import { PropertyService } from 'src/app/services/property.service';
-import jspdf from 'jspdf';  
+import jspdf from 'jspdf';
 import html2canvas from 'html2canvas';
+import { forkJoin } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
 
-declare var jsPDF:any;
+declare var jsPDF: any;
 
 @Component({
   selector: 'app-property-details',
@@ -58,56 +60,82 @@ export class PropertyDetailsComponent implements OnInit {
     { name: '2 cars' },
     { name: '2+' },
   ];
+
+  floorPlanImage: any;
+  coverImage: any;
+  isShow: boolean = true;
   constructor(public admin: AdminService, private route: ActivatedRoute,
     private spinner: NgxSpinnerService,
     public constant: Constant,
-    private propertyService: PropertyService,private router: Router,) { }
+    private propertyService: PropertyService, private router: Router,) { }
 
   ngOnInit() {
     this.property = this.propertyService.property;
-    this.route.params.subscribe( params => {
+    this.route.params.subscribe(params => {
       this.getPropertyDetails(params.property_id);
+      this.getBasicDetails(params.property_id);
     });
     // this.cs.propertyDetailsData$.subscribe(res => {
     //   this.data = res;
     // });
   }
-  goBack(){ 
-    this.router.navigate(['/dashboard/properties/view-properties', {for: 'back'}])
-  }
+  // goBack(){ 
+  //   this.router.navigate(['/dashboard/properties/view-properties', {for: 'back'}])
+  // }
   getPropertyDetails(property_id: string) {
     let self = this;
     // this.spinner.show();
-    this.admin.postDataApi('getPropertyById', {property_id: property_id})
+    this.admin.postDataApi('getPropertyById', { property_id: property_id })
       .subscribe(success => {
         this.spinner.hide();
         this.property = success.data;
-        if(this.property.property_parking_space && this.property.property_parking_space.length > 0){
-          this.property.property_parking_space.forEach(item=>{
-          self.parking_count = self.parking_count + item.parking_count;
+        if (this.property.property_parking_space && this.property.property_parking_space.length > 0) {
+          this.property.property_parking_space.forEach(item => {
+            self.parking_count = self.parking_count + item.parking_count;
           })
-         }
+        }
         this.amenities_string = this.property.amenities.map(r => { return r.name }).join(',');
-       this.marital_statuses = this.property.marital_statuses.map(r => { return r.name }).join(', ');
+        this.marital_statuses = this.property.marital_statuses.map(r => { return r.name }).join(', ');
       }, error => {
         this.spinner.hide();
       });
   }
 
-  downloadPDF(){
-    var data = document.getElementById('contentToConvert');  //Id of the table
-      html2canvas(data).then(canvas => {  
+  getBasicDetails = (property_id: string): void => {
+    this.spinner.show();
+    forkJoin([
+      this.admin.postDataApi('getFloorPlanImage', { id: property_id || '' }),
+      // this.adminService.postDataApi('getCreditsDeadlines', {id:property_id || ''}),
+      this.admin.postDataApi('getCoverImage', { id: property_id || '' })
+    ]).subscribe((response: any[]) => {
+      this.spinner.hide();
+      this.floorPlanImage = 'data:image/jpeg;base64,' + response[0].data
+      this.coverImage = 'data:image/jpeg;base64,' + response[1].data
+      // this.PaymentScheme = response[2].data
+    });
+  }
+
+  downloadPDF() {
+    this.spinner.show();
+    this.isShow = false;
+    setTimeout(() => {
+      var data = document.getElementById('contentToConvert');  //Id of the table
+      html2canvas(data).then(canvas => {
+        this.isShow = true;
         // Few necessary setting options  
-        let imgWidth = 208;   
-        let pageHeight = 295;    
-        let imgHeight = canvas.height * imgWidth / canvas.width;  
-        let heightLeft = imgHeight;  
-  
-        const contentDataURL = canvas.toDataURL('image/png')  
+        let imgWidth = 208;
+        let pageHeight = 295;
+        let imgHeight = canvas.height * imgWidth / canvas.width;
+        let heightLeft = imgHeight;
+
+        const contentDataURL = canvas.toDataURL('image/png')
         let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF  
-        let position = 0;  
-        pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)  
-        pdf.save('property-details.pdf'); // Generated PDF   
+        let position = 0;
+        pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
+        pdf.save('property-details.pdf'); // Generated PDF  
+        this.spinner.hide();
       });
+    }, 500);
+
   }
 }
