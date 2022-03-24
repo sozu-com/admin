741753ab25c0a34c0de4789c95248bcc8c32f71d
somya <somapuri63@gmail.com>
2022-03-24 17:59:35 +0530

    esternal test

--- src/app/models/acl.model.ts
+++ src/app/models/acl.model.ts
@@ -41,7 +41,9 @@ export class ACL {
     image?: string;
     img_loader?: boolean;
     email?: string;
+    admin_estend?: Array<Permission> = [];
     admin_acl?: Array<Permission> = [];
+    adminAcls?: Array<Permission> = [];
     address?: Array<NewAddress>;
     user_type?: any;
     is_cordinator?: any;
